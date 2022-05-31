41c44efc0d17b33db72823fc0546dc42d3566014
Onur <onurbelek@outlook.com>
2022-05-31 16:20:41 +0100

    changed it for heroku

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // importing the dependencies
 const express = require('express');
-const userRoute = require('../routes/user.route');
-const adminRoute = require('../routes/admin-main.route');
-const productRoute = require('../routes/product.route');
-const DB = require('../configs/database');
-const env = require('../configs/env-configs')
+const userRoute = require('./routes/user.route');
+const adminRoute = require('./routes/admin-main.route');
+const productRoute = require('./routes/product.route');
+const DB = require('./configs/database');
+const env = require('./configs/env-configs')
 
 // Env config
 env.config();
